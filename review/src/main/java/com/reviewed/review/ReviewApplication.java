a0c349a6501de9d594ceeb82ddf923479ad18ae7
pawan sharma <pa.wonsharma7@gmail.com>
2022-08-14 19:38:23 +0530

    initial

---
+++ review/src/main/java/com/reviewed/review/ReviewApplication.java
@@ -0,0 +1,13 @@
+package com.reviewed.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+
+}
